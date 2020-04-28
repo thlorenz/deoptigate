888c0b55b94d1caf9d7ef1c1fe6eb76f754aa1f3
Andre Wiggins <andrewiggins@live.com>
2020-04-28 09:38:42 -0700

    Validate property after accessing it

--- test/deoptigate.log.test.js
+++ test/deoptigate.log.test.js
@@ -49,18 +49,21 @@ test('adders.v8.log', async (t) => {
   const destLogPath = await prepareLogFile(srcLogPath, replacements)
 
   const result = await deoptigateLog(destLogPath)
-  const fileData = result.get(addersSrcFile)
-  const deoptLocation = fileData.deoptLocations[0]
-  const deoptData = fileData.deopts.get(deoptLocation)
-  const updateData = deoptData.updates[2]
-
   t.equal(result.size, 1, 'number of files')
+
+  const fileData = result.get(addersSrcFile)
   t.equal(fileData.fullPath, addersSrcFile, 'fullPath')
   t.equal(fileData.ics.size, 33, 'number of ics')
   t.equal(fileData.deopts.size, 7, 'number of deopts')
   t.equal(fileData.codes.size, 16, 'number of codes')
+
+  const deoptLocation = fileData.deoptLocations[0]
   t.equal(deoptLocation, 'addAny:93:27', 'first deoptLocation')
+
+  const deoptData = fileData.deopts.get(deoptLocation)
   t.equal(deoptData.file, addersSrcFile, 'deopt file path')
+
+  const updateData = deoptData.updates[2]
   t.equal(updateData.bailoutType, 'eager', 'deopt update bailout type')
 })
 
@@ -83,17 +86,20 @@ test('two-modules.v8.log', async (t) => {
   const destLogPath = await prepareLogFile(srcLogPath, replacements)
 
   const result = await deoptigateLog(destLogPath)
-  const fileData = result.get(srcFile)
-  const icLocation = fileData.icLocations[0]
-  const icData = fileData.ics.get(icLocation)
-  const updateData = icData.updates[0]
-
   t.equal(result.size, 2, 'number of files')
+
+  const fileData = result.get(srcFile)
   t.equal(fileData.fullPath, srcFile, 'fullPath')
   t.equal(fileData.ics.size, 25, 'number of ics')
   t.equal(fileData.deopts.size, 0, 'number of deopts')
   t.equal(fileData.codes.size, 8, 'number of codes')
+
+  const icLocation = fileData.icLocations[0]
   t.equal(icLocation, 'Object1:3:12', 'first icLocation')
+
+  const icData = fileData.ics.get(icLocation)
   t.equal(icData.file, srcFile, 'ics file path')
+
+  const updateData = icData.updates[0]
   t.equal(updateData.map, '37cdf3b7a811', 'ics update map')
 })
